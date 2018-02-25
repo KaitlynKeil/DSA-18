0741879802c157d3b8d77416d67bf6a5ccb31deb
KaitlynKeil <kaitly.keil@students.olin.edu>
2018-02-25 08:44:07 -0500

    Updating all my homework

--- sorting/day02/src/LocksAndKeys.java
+++ sorting/day02/src/LocksAndKeys.java
@@ -6,11 +6,44 @@ public class LocksAndKeys {
         A[j] = t;
     }
     static char[][] locksAndKeys(char[] locks, char[] keys) {
-        // TODO: Return a 2d char array, where result[0] is the sorted locks, and result[1] is the sorted keys
         char[][] result = new char[2][];
+        lockAndKeySort(keys, locks, 0, keys.length-1);
+        lockAndKeySort(locks, keys, 0, locks.length-1);
         result[0] = locks;
         result[1] = keys;
         return result;
+    }
+
+    public static void lockAndKeySort(char[] a1, char[] a2, int lo, int hi) {
+        if (lo < hi) {
+            int p = pivot(a1, a2, lo, hi);
+            lockAndKeySort(a1, a2, lo, p-1);
+            lockAndKeySort(a1, a2, p+1, hi);
+        }
+    }
+
+    public static int pivot(char[] a1, char[] a2, int lo, int hi) {
+        if(a2.length <= 1) return lo;
+        int pivot = a2[lo];
+        int current_piv = lo;
+        int end = hi;
+
+        for(int i = lo; i <= end; i++){
+            if(a1[i] > pivot) {
+                swap(a1, i, end);
+                end--;
+                i--;
+            }
+            else if(a1[i] == pivot) {
+                current_piv = i;
+            }
+            else{
+                swap(a1, i, current_piv);
+                current_piv = i;
+            }
+        }
+        swap(a2, lo, current_piv);
+        return current_piv;
     }
 }
 
